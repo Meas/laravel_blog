60a9a984fc6dc921698d60f81c5f35552971059c
FedjaD <durmicfedja@gmail.com>
2016-02-17 17:18:01 +0100

    aa

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -4,10 +4,12 @@
 	<article>
 		 {{ $article->body }}
 	</article><br>
+	@unless($article-tags->isEmpty())
 	<h5>Tags:</h5>
 	<ul>
 	@foreach($article->tags as $tag)
 		<li> {{ $tag->name }}</li>
 	@endforeach
 	</ul>
+	@endunless
 @stop
